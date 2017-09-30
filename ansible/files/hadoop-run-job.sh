dac7fd8fbcb65bee3a921b5f0d66d9e45315779d
akiroz <akiroz.vectis@gmail.com>
2017-10-01 07:18:44 +0800

    AWS fixes

--- ansible/files/hadoop-run-job.sh
+++ ansible/files/hadoop-run-job.sh
@@ -4,6 +4,8 @@ JAR="$1"
 CLASS="$2"
 IN="$3"
 OUT="/$2-out"
+export HADOOP_HOME=/home/hadoop/hadoop-2.7.3
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
 hadoop fs -rm -r $OUT || true
 hadoop jar "$JAR" "$CLASS" "$IN" "$OUT"
 hadoop fs -cat "$OUT/part-r-00000"
--- compile-and-run.sh
+++ compile-and-run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
+
 [[ $1 ]] || echo "No main class supplied."
 [[ $2 ]] || echo "No input path supplied."
 CLASS="$1"
 IN="$2"
 shift; shift
+
+NAMENODE=54.153.23.91
+PORT=22
+
 gradle jar
-scp -i keys/vm1-hadoop -P 12231 build/libs/assg1.jar hadoop@137.189.89.214:
-ssh -i keys/vm1-hadoop -p 12231 hadoop@137.189.89.214 "./hadoop-run-job.sh assg1.jar $CLASS $IN $@"
+scp -i keys/vm1-hadoop -P $PORT build/libs/assg1.jar "hadoop@$NAMENODE:"
+ssh -i keys/vm1-hadoop -p $PORT "hadoop@$NAMENODE" "./hadoop-run-job.sh assg1.jar $CLASS $IN $@"
