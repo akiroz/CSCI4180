756c5149e90299ca47518861306e8d42c11a1400
dddwong <1155048371@link.cuhk.edu.hk>
2017-10-19 03:47:27 +0000

    Remove file BOM

--- src/main/java/NgramCount.java
+++ src/main/java/NgramCount.java
@@ -1,4 +1,4 @@
-﻿import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
