81d7af3456a0f41592205d9e1690bc17751642dd
akiroz <akiroz.vectis@gmail.com>
2017-10-20 11:00:03 +0800

    pass additional args to hadoop job

--- ansible/files/hadoop-run-job.sh
+++ ansible/files/hadoop-run-job.sh
@@ -6,6 +6,7 @@ IN="$3"
 OUT="/$2-out"
 export HADOOP_HOME=/home/hadoop/hadoop-2.7.3
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+shift; shift; shift
 hadoop fs -rm -r $OUT || true
-hadoop jar "$JAR" "$CLASS" "$IN" "$OUT"
+hadoop jar "$JAR" "$CLASS" "$IN" "$OUT" "$@"
 hadoop fs -cat "$OUT/part-r-00000"
